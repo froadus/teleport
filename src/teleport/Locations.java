d0a0bbc8aad74fc441da336fda7d4290f5bf6dae
Froadus <simjoh99@gmail.com>
2017-02-24 17:17:37 +0100

    Uppdaterade filer

--- src/teleport/Locations.java
+++ src/teleport/Locations.java
@@ -1,18 +1,27 @@
 package teleport;
 
+import org.bukkit.Location;
+
 import java.io.Serializable;
-import java.util.Set;
 
 @SuppressWarnings("serial")
-public class Location implements Serializable {
+public class Locations implements Serializable {
+    public String name;
 
-        public String name;
+    public double x;
+    public double y;
+    public double z;
+    public float yaw;
+    public float pitch;
 
-	 	public Location location;
-	    
-	    public Location(String name, Location location) {
-	        this.name = name;
+    public Locations(String name, Location location) {
+        this.name = name;
 
-	        this.location = location;
-	    }
+        this.x = location.getX();
+        this.y = location.getY();
+        this.z = location.getZ();
+
+        this.yaw = location.getYaw();
+        this.pitch = location.getPitch();
+    }
 }
--- src/teleport/Main.java
+++ src/teleport/Main.java
@@ -1,5 +1,8 @@
 package teleport;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -14,18 +17,15 @@ public class Main extends JavaPlugin {
 
 	private static Main instance;
 
-	public static List<ProtectedArea> area;
+	public static List<Locations> area;
 	public static long saveTime = System.currentTimeMillis();
-
-	public static Map<String, InProgressArea> inProgressAreas = new HashMap<>();
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public void onEnable(){
 		instance = this;
 
-		getLogger().info("Froadus plugin startades");
-		new PlayerListener(this);
+		getLogger().info("Locations startades");
 
 		File dir = getDataFolder();
 
@@ -35,7 +35,7 @@ public class Main extends JavaPlugin {
 			}
 		}
 
-		Main.area = (List<ProtectedArea>) load(new File(getDataFolder(), "protected_areas.dat"));
+		Main.area = (List<Locations>) load(new File(getDataFolder(), "platser.dat"));
 
 		if (Main.area == null) {
 			Main.area = new ArrayList<>();
@@ -69,76 +69,75 @@ public class Main extends JavaPlugin {
 
 	@Override
 	public void onDisable() {
-		save(Main.area, new File(getDataFolder(), "protected_areas.dat"));
+		save(Main.area, new File(getDataFolder(), "platser.dat"));
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (command.getName().equalsIgnoreCase("newcuboid") && sender instanceof Player) {
+		if (command.getName().equalsIgnoreCase("w") && sender instanceof Player) {
 			Player p = (Player) sender;
 
-			String owner = sender.getName();
-
-			Set<String> users = Arrays
-					.stream(args)
-					.filter(s -> !s.equals(owner))
-					.collect(Collectors.toSet());
-
-			inProgressAreas.put(owner, new InProgressArea(owner, users));
-
-			p.sendMessage("Välj den första punkten för din area");
-
-			return true;
-		}
-
-		if (command.getName().equalsIgnoreCase("listcuboid") && sender instanceof Player) {
-			Player p = (Player) sender;
-
-			if (Main.area.isEmpty()) {
-				p.sendMessage("Det finns inga areor!");
-			} else {
+			if (args.length > 0) {
 				for (int i = 0; i < Main.area.size(); i++) {
-					ProtectedArea area = Main.area.get(i);
-
-					String msg = MessageFormat.format("Area nr {0}: Ägare: {1}, Åtkomst: {2}, Plats(xyz,xyz): {3} / {4} / {5} / {6} / {7} / {8}",
-							i + 1,
-							area.owner,
-							area.users.stream().collect(Collectors.joining(", ")),
-							area.x1,
-							area.y1,
-							area.z1,
-							area.x2,
-							area.y2,
-							area.z2);
-
-					p.sendMessage(msg);
+					Locations a = Main.area.get(i);
+					if (a.name.equalsIgnoreCase(args[0])) {
+					    Location loc = new Location(p.getWorld(),a.x,a.y,a.z,a.yaw,a.pitch);
+						p.teleport(loc);
+                        p.sendMessage(ChatColor.GREEN + "Teleporterade till: " + ChatColor.GOLD + a.name);
+						return true;
+					}
 				}
 			}
-
+            p.sendMessage("Platsen finns inte!");
 			return true;
 		}
 
-		if (command.getName().equalsIgnoreCase("remcuboid") && sender instanceof Player) {
+		if (command.getName().equalsIgnoreCase("wadd") && sender instanceof Player) {
 			Player p = (Player) sender;
-			if (args.length == 1) {
-				int id = Integer.parseInt(args[0])-1;
-				if (id < Main.area.size() && id >= 0) {
-					Main.area.remove(id);
-
-					if ((System.currentTimeMillis() - Main.saveTime) > 1000) {
-						save(Main.area, new File(getDataFolder(), "protected_areas.dat"));
-						Main.saveTime = System.currentTimeMillis();
-					}
-					p.sendMessage("Den skyddade ytan " + args[0] + " har tagits bort!");
-					return true;
+			if (args.length > 0) {
+			    for (int i = 0; i < Main.area.size(); i++) {
+			        if (args[0].equalsIgnoreCase(args[0])) {
+                        p.sendMessage("Namnet änvänds redan!");
+			            return true;
+                    }
+                }
+				Main.area.add(new Locations(args[0], p.getLocation()));
+                p.sendMessage(ChatColor.GREEN + "Ny plats tillagd: " + ChatColor.GOLD + args[0]);
+				if ((System.currentTimeMillis() - Main.saveTime) > 1000) {
+					save(Main.area, new File(getDataFolder(), "platser.dat"));
+					Main.saveTime = System.currentTimeMillis();
 				}
+				return true;
+			} else {
+				return false;
 			}
+		}
+
+		if (command.getName().equalsIgnoreCase("wlist") && sender instanceof Player) {
+			Player p = (Player) sender;
+			p.sendMessage(ChatColor.YELLOW + "--------- " + ChatColor.WHITE + "Platser för teleportering " + ChatColor.YELLOW + "---------------------");
+			p.sendMessage(ChatColor.GRAY + "För teleportering, använd: /w [platsnamn]");
+			for (int i = 0; i < Main.area.size(); i++) {
+				p.sendMessage(ChatColor.GOLD + area.get(i).name);
+			}
+			return true;
+		}
+
+		if (command.getName().equalsIgnoreCase("wrem") && sender instanceof Player) {
+            Player p = (Player) sender;
+            if (args.length > 0) {
+                for (int i = 0; i < Main.area.size(); i++) {
+                    String name = Main.area.get(i).name;
+                    if (name.equalsIgnoreCase(args[0])) {
+                        Main.area.remove(i);
+                        p.sendMessage(ChatColor.GREEN + "Plats borttagen: " + ChatColor.GOLD + name);
+                        return true;
+                    }
+                }
+            }
+			return false;
 		}
 
 		return false;
-	}
-
-	public static Main getInstance() {
-		return instance;
 	}
 }
